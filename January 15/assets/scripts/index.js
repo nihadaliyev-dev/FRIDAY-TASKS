f330e6d1df74ace5040c37ec8a95f5702b5e25f0
Nihad Aliyev <nihadaliyev.530@gmail.com>
2025-01-15 14:26:04 +0400

    January 15 Task || Done -- 100% || Nihad Aliyev

--- January 15/assets/scripts/index.js
+++ January 15/assets/scripts/index.js
@@ -7,40 +7,44 @@ export function createCards(arr) {
   if (arr.length > 0) {
     wrapper.innerHTML = "";
     arr.forEach((country) => {
-      const card = document.createElement("div");
+      // AZE !
+      if (country.capital != "Yerevan") {
+        const card = document.createElement("div");
 
-      const cardFlagDiv = document.createElement("div");
-      const cardFlag = document.createElement("img");
-      const cardContentMain = document.createElement("div");
-      const cardContentHeader = document.createElement("div");
-      const cardContentBody = document.createElement("div");
+        const cardFlagDiv = document.createElement("div");
+        const cardFlag = document.createElement("img");
+        const cardContentMain = document.createElement("div");
+        const cardContentHeader = document.createElement("div");
+        const cardContentBody = document.createElement("div");
 
-      const cardContentCapital = document.createElement("div");
-      const cardContentRegion = document.createElement("div");
-      const cardContentPopulation = document.createElement("div");
-      cardFlag.setAttribute("src", country.flags.svg);
+        const cardContentCapital = document.createElement("div");
+        const cardContentRegion = document.createElement("div");
+        const cardContentPopulation = document.createElement("div");
+        cardFlag.setAttribute("src", country.flags.svg);
 
-      cardContentHeader.textContent = `${country.name.official}`;
-      cardContentCapital.textContent = `Capital: ${country.capital}`;
-      cardContentRegion.textContent = `Region: ${country.region}`;
-      cardContentPopulation.textContent = `Population: ${country.population}`;
+        cardContentHeader.textContent = `${country.name.official}`;
+        cardContentCapital.textContent = `Capital: ${country.capital}`;
+        cardContentRegion.textContent = `Region: ${country.region}`;
+        cardContentPopulation.textContent = `Population: ${country.population}`;
 
-      cardFlagDiv.append(cardFlag);
-      cardContentBody.append(
-        cardContentCapital,
-        cardContentRegion,
-        cardContentPopulation
-      );
-      cardContentMain.append(cardContentHeader, cardContentBody);
+        cardFlagDiv.append(cardFlag);
+        cardContentBody.append(
+          cardContentCapital,
+          cardContentRegion,
+          cardContentPopulation
+        );
+        cardContentMain.append(cardContentHeader, cardContentBody);
 
-      cardFlag.className = "aspect-video object-cover shadow-sm";
-      cardContentMain.className =
-        "p-3 group-hover:absolute group-hover:top-0 transition-all ease-in duration-300 flex flex-col items-start justify-end group-hover:text-white group-hover:bg-[#00000080] group-hover:w-full group-hover:h-full card__content";
-      cardContentBody.className = "hidden group-hover:flex flex-col";
-      card.className = "shadow-lg rounded-lg overflow-hidden relative group";
+        cardFlag.className = "aspect-video object-cover shadow-sm";
+        cardContentMain.className =
+          "p-3 group-hover:absolute group-hover:top-0 transition-all ease-in duration-300 flex flex-col flex-col-reverse items-start justify-start group-hover:text-white group-hover:bg-[#00000080] group-hover:w-full group-hover:h-full card__content";
+        cardContentBody.className = "hidden group-hover:flex flex-col";
+        card.className =
+          "shadow-lg dark:shadow-[#fbfbfb20] dark:hover:shadow-[#fbfbfb50] transition-all ease-in duration-300 rounded-lg overflow-hidden relative group";
 
-      card.append(cardFlagDiv, cardContentMain);
-      wrapper.append(card);
+        card.append(cardFlagDiv, cardContentMain);
+        wrapper.append(card);
+      }
     });
   } else {
     wrapper.innerHTML = `There's no country named ${searchInput.value}`;
@@ -68,4 +72,9 @@ filterRegion.addEventListener("change", (e) => {
   } else {
     getCountries();
   }
+});
+
+const themeBtn = document.querySelector(".theme__toggle");
+themeBtn.addEventListener("click", (e) => {
+  document.className = "dark";
 });
